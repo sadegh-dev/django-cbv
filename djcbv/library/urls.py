e5768a08b5711427a6bd11af97d4cdf35e36a97f
charlie <sadegh.rez@gmail.com>
2021-05-18 20:47:46 +0430

    detail book

--- djcbv/library/urls.py
+++ djcbv/library/urls.py
@@ -6,4 +6,6 @@ app_name = 'library'
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('h', views.Home2.as_view(), name='home2'),
+    path('list-books', views.List_books.as_view(), name='list_books'),
+    path('detail-book/<int:pk>/', views.Detail_book.as_view(), name='detail_book'),
 ]
--- djcbv/library/views.py
+++ djcbv/library/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from .models import Book
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 
 class Home(View):
     def get(self, request, *args, **kwargs):
@@ -12,6 +15,7 @@ class Home(View):
         return render(request, 'library/home.html', context)
 
 
+
 class Home2(TemplateView):
     template_name = 'library/home2.html'
 
@@ -19,6 +23,37 @@ class Home2(TemplateView):
         context = super().get_context_data(**kwargs)
         context ['books'] = Book.objects.all()
         return context
+
+
+
+class List_books(ListView):
+    #-- address default : 'appname/modelname_list.html'
+    #-- address default : 'library/book_list.html'
+    template_name = 'library/list_books.html'
+    model = Book
+    ordering = ['-created']
+
+    #-- set query set
+    #queryset = Book.objects.filter(...)
+
+    #-- set complex queryset
+    #def get_queryset(self) :
+    #   books = Book.objects.filter(...)
+    #   return books
+
+    #-- change object_list -to- allbooks in template file
+    #context_object_name = 'allbooks'
+
+
+
+class Detail_book(DetailView):
+    #-- address default : 'library/book_detail.html'
+    template_name = 'library/detail_book.html'
+    model = Book
+    context_object_name = 'book'
+
+
+
 
 
 
