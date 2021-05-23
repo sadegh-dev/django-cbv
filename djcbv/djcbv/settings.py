3420c527ec6a9789d74375870822fb26b5dd908c
charlie <sadegh.rez@gmail.com>
2021-05-23 12:51:01 +0430

    connect to email service

--- djcbv/djcbv/settings.py
+++ djcbv/djcbv/settings.py
@@ -128,3 +128,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGIN_REDIRECT_URL = 'library:list_books'
+
+
+# Connect to stmp
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'myemail.gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Django - cbv <myemail.gmail.com> '
+
+
+
