6f4a61895a4f7f3ad05d5dfc5c660470472edf0b
sadegh-dev <sadegh.rez@gmail.com>
2022-08-01 11:05:03 +0430

    insert data from buffer in project

--- core/library/urls.py
+++ core/library/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
+
 
 app_name = 'library'
 
 urlpatterns = [
-    #...
+    path('view-class/', views.Home.as_view())
 ]
--- core/library/views.py
+++ core/library/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class Home(View):
+
+    http_method_names= ['get', 'options']
+
+    def get(self, request):
+        return render(request, 'home.html')
+    
+    def options(self, request, *args, **kwargs):
+        response = super().options(request, *args, **kwargs)
+        response.headers['host'] = 'localhost'
+        response.headers['user'] = request.user
+        return response
+    
+    def http_method_not_allowed(self, request, *args, **kwargs) :
+        super().http_method_not_allowed(request, *args, **kwargs)
+        return render(request, 'http_method_not_allowed.html')
